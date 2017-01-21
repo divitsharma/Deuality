428248b8b5afa7a7e1fa9ce2ed49f2dd18d28689
Divit Sharma <Divit Sharma>
2017-01-21 11:19:12 -0500

    Added game

---
+++ Wave Guy Thing/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform camera;
+
+    // Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(0.1f, 0, 0);
+        camera.transform.position = new Vector3(this.transform.position.x, 0, -10);
+	}
+}
---
+++ Wave Guy Thing/Assets/Scripts/WaveParticle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveParticle : MonoBehaviour {
+
+    public float amplitude;
+    public float factor;
+
+    // Use this for initialization
+	void Start () {
+		//time = Time.time
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //this.transform.position += new Vector3(0.01f, 0, 0);
+        //this.transform.Translate(0.1f, 0, 0);
+        this.transform.Translate(0, amplitude*Mathf.Sin(Time.time / factor), 0);
+        //x = sin(t)
+        print(amplitude * Mathf.Sin(Time.time));
+    }
+}
